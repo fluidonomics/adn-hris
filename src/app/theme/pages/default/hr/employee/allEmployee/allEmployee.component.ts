1b65abda9a43e21635b924b8bce163a4cf0ff92b
Sumit Mahajan <sumit.mahajan@dahlia-tech.com>
2018-05-08 13:13:54 +0530

    html changes of all employee

--- src/app/theme/pages/default/hr/employee/allEmployee/allEmployee.component.ts
+++ src/app/theme/pages/default/hr/employee/allEmployee/allEmployee.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/co
 import { Helpers } from '../../../../../../helpers';
 import { ScriptLoaderService } from '../../../../../../_services/script-loader.service';
 import { CommonService } from '../../../../../../base/_services/common.service';
+import { AuthService } from "../../../../../../base/_services/authService.service";
 import { HrService } from '../../hr.service';
 declare var $;
 
@@ -16,14 +17,20 @@ export class AllEmployeeComponent implements OnInit, AfterViewInit {
     reverse: boolean = false;
     p2: number = 1;
     search: any;
+    _currentEmpId:number;
    
 
 
     constructor(private _script: ScriptLoaderService,
-        private _hrService: HrService) {
+        private _hrService: HrService,
+        public _authService: AuthService) {
 
     }
     ngOnInit() {
+        this._authService.validateToken().subscribe(
+            res => {
+                this._currentEmpId = this._authService.currentUserData._id;
+            });
         this.loadAllEmployee();
     }
 
@@ -33,7 +40,14 @@ export class AllEmployeeComponent implements OnInit, AfterViewInit {
     loadAllEmployee() {
         this._hrService.getAllEmployee()
             .subscribe(
-            data => {
+            res => {
+                let data= res.json().data || [];
+                if(data.length >0)
+                {
+                    data = data.filter(obj => obj.HrScope_Id== this._currentEmpId);
+                    this.employeeData = data || [];
+                }
+                else
                 this.employeeData = data.json().data || [];
             },
             error => {
