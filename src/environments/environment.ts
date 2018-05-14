6d70558580ea6cddffaf9bae45e4ba9a553c7928
fluidonomics <dev@fluidonomics.com>
2018-05-14 11:30:52 +0530

    live

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@
 export const environment = {
     production: false,
     api_base: {
-        apiBase: "http://localhost:3000",
-       //apiBase: "http://goforship.com:3000",
+        //apiBase: "http://localhost:3000",
+        apiBase: "http://goforship.com:3000",
         apiPath: "api",
         resetPasswordCallback: "",
         signInPath: 'auth/login',
