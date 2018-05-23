8e2eb0d7fa861530988898d068245901371b138d
Sumit Mahajan <sumit.mahajan@dahlia-tech.com>
2018-05-23 19:44:37 +0530

    change of kra

---
+++ src/app/theme/pages/default/my/workflows/kra/kra.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { Headers, Http, RequestOptions, Response } from "@angular/http";
+import { environment } from '../../../../../../../environments/environment'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { AuthService } from "../../../../../../base/_services/authService.service";
+
+@Injectable()
+export class KraService {
+    constructor(private authService: AuthService) {
+    }
+
+    getEmployeeKraWorkFlowInfo(emp_id?: number): Observable<Response> {
+        let url = "kra/getEmployeeKraWorkFlowInfo?emp_id=" + emp_id;
+        return this.authService.get(url).map(this.extractData).catch(this.handleError);
+    }
+
+    private extractData(res: Response) {
+        return res || {};
+    }
+
+    private handleError(error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        return Observable.throw(errMsg);
+    }
+
+}
