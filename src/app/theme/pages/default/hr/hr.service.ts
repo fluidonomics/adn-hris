69a6a312ef2ef465e073d8a27d6baefd6a87724e
Sumit Mahajan <sumit.mahajan@dahlia-tech.com>
2018-05-23 14:41:54 +0530

    change of html tabel

--- src/app/theme/pages/default/hr/hr.service.ts
+++ src/app/theme/pages/default/hr/hr.service.ts
@@ -2,7 +2,8 @@ import { Injectable, } from "@angular/core";
 import { Headers, Http, RequestOptions, Response } from "@angular/http";
 import { environment } from '../../../../../environments/environment'
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 import { AuthService } from "../../../../base/_services/authService.service"
 
 @Injectable()
