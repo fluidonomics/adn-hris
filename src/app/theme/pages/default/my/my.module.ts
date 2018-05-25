f67e38ac85f435eeea2af2596264a6beaaab5887
unknown <sumit.mahajan@dahlia-tech.com>
2018-05-26 00:29:26 +0530

    change of routing and add missing component

--- src/app/theme/pages/default/my/my.module.ts
+++ src/app/theme/pages/default/my/my.module.ts
@@ -1,36 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-
-//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { ModalModule } from 'ngx-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LayoutModule } from '../../../layouts/layout.module';
-import { MyComponent } from './my.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ProfileEditComponent } from './profile-edit/profile-edit.component';
-import { MyLearningComponent } from './workflows/learning/learning.component'
-import { MyPapComponent } from './workflows/pap/pap.component'
-import { MyPipComponent } from './workflows/pip/pip.component'
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { ModalModule } from 'ngx-bootstrap';
 import { NgxPaginationModule } from 'ngx-pagination';
-//import { ReviewerComponent } from './team/workflows/reveiwer/reviewer.component';
-//import { SupervisorComponent } from './team/workflows/supervisor/supervisor.component';
-
-
 import { MyRoutingModule } from './my-routing.module';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { NgUploaderModule } from 'ngx-uploader';
-import { MyService } from "./my.service"
 import { BaseModule } from '../../../../base/base.module';
 import { LeaveModule } from './leaves/leave.module';
+
+import { MyComponent } from './my.component';
+
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileEditComponent } from './profile-edit/profile-edit.component';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
+
+// import { MyLearningComponent } from './workflows/learning/learning.component';
+// import { MyPapComponent } from './workflows/pap/pap.component';
+// import { MyPipComponent } from './workflows/pip/pip.component';
+
+import { DocumentsComponent } from './documents/documents.component';
+
+import { MyService } from "./my.service"
+
 
 @NgModule({
     imports: [
         CommonModule,
         LayoutModule,
-        //NgbModule.forRoot(),
         NgxPaginationModule,
         FormsModule,
         ReactiveFormsModule,
@@ -44,12 +45,14 @@ import { LeaveModule } from './leaves/leave.module';
     ], declarations: [
         MyComponent,
         ProfileComponent,
-        MyLearningComponent,
-        MyPapComponent,
-        MyPipComponent,
         ProfileEditComponent,
-        //ReviewerComponent,
-        //SupervisorComponent
+        ProfileViewComponent,
+
+        // MyLearningComponent,
+        // MyPapComponent,
+        // MyPipComponent,
+       
+        DocumentsComponent
     ],
     providers: [MyService]
 })
