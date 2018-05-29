acbca10abf92abde120e1d51c96d1d6cf2e4af8c
fluidonomics <dev@fluidonomics.com>
2018-05-29 19:58:25 +0530

    employee dashboard

--- src/app/theme/pages/default/hr/dashboard/dashboard.component.ts
+++ src/app/theme/pages/default/hr/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { Meta, Title } from "@angular/platform-browser";
 import { FormBuilder } from "@angular/forms";
+import { Helpers } from '../../../../../helpers';
+import { ScriptLoaderService } from '../../../../../_services/script-loader.service';
 //import { ModalDismissReasons, NgbDateStruct, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 const now = new Date();
@@ -10,5 +14,24 @@ const now = new Date();
     encapsulation: ViewEncapsulation.None,
     styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, AfterViewInit {
+    constructor( @Inject(PLATFORM_ID) private platformId: Object,
+    meta: Meta, title: Title,
+    private _script: ScriptLoaderService,
+) {
+    title.setTitle('ADN Dashbord | Dashboard');
+    meta.addTags([
+        { name: 'author', content: '' },
+        { name: 'keywords', content: 'Dashboard' },
+        { name: 'description', content: 'Dashboard.' }
+    ]);
+}
+
+ngOnInit() {
+
+}
+ngAfterViewInit() {
+    this._script.load('.m-grid__item.m-grid__item--fluid.m-wrapper',
+        'assets/app/js/dashboard.js');
+}
 }
