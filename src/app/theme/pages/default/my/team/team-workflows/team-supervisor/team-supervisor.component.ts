fd6eeba1e13bc7ace11c77665df3d2c23d69f2c4
Sumit Mahajan <sumit.mahajan@dahlia-tech.com>
2018-06-18 19:54:56 +0530

    change for kra dashboard

--- src/app/theme/pages/default/my/team/team-workflows/team-supervisor/team-supervisor.component.ts
+++ src/app/theme/pages/default/my/team/team-workflows/team-supervisor/team-supervisor.component.ts
@@ -16,6 +16,7 @@ export class MyTeamSupervisorComponent implements AfterViewInit {
     kraData:any=[];
     imageBase:any;
     kraReverse:boolean=true;
+    employeeReverse:boolean=true;
 
     constructor(
         private _myteamService: MyTeamService,
