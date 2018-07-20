e078a0d72c4cde989237feec23ac68c3ac31777e
sudarshanbanote <techsud95@gmail.com>
2018-07-20 12:24:36 +0530

    Leave UI

--- src/app/theme/pages/default/hr/email/broadcast/broadcast.component.ts
+++ src/app/theme/pages/default/hr/email/broadcast/broadcast.component.ts
@@ -6,7 +6,9 @@ const now = new Date();
 
 @Component({
     selector: ".m-grid__item.m-grid__item--fluid.m-wrapper",
-    templateUrl: "./broadcast.component.html",
+    //templateUrl: "./broadcast.component.html", // employee/viewAll
+    templateUrl: "./broadcast.component.1.html", //employee
+    //templateUrl: "./broadcast.component.2.html", // Supervisor/ViewAll
     encapsulation: ViewEncapsulation.None,
 })
 export class BroadCastComponent {
