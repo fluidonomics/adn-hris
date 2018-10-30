825133c2484a025422d2f685f1ab765d2d9b3f2e
ASHISH RAO SHIRSAGAR <SHIRSAGARGROUPS11@gmail.com>
2018-10-31 03:38:42 +0530

    remove comment code

--- src/app/theme/pages/default/hr/hr.service.ts
+++ src/app/theme/pages/default/hr/hr.service.ts
@@ -27,16 +27,6 @@ export class HrService {
     }
 
     getKRAReport(){
-        /* let headers = new Headers();
-        headers.append('Accept', 'text/csv');
-        let url = "kra/getKRA_Report";
-        return this.authService.get(url, { headers: headers, responseType: ResponseContentType.Blob }).map(this.extractData).catch(this.handleError); */
-
-        /* let headers = new Headers();
-        headers.append('Accept', 'application/pdf'); 
-        let url = this.CONTENT_API_BASE + fileUrl;
-        return this._http.get(url, { headers: headers, responseType: ResponseContentType.Blob }).catch(this.handleError); */
-
         let url = "kra/getKRA_Report";
         return this.authService.get(url).map(this.extractData).catch(this.handleError);
     }
