a0ae5240d4336563b33ba9723686e66ebd715b51
Murtaza <murtazamunshi@live.com>
2018-12-07 20:37:46 +0530

    Created components

---
+++ src/app/theme/pages/default/my/team/team-workflows/team-supervisor/mtr-view/mtr-view.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'mtr-view',
+    templateUrl: 'mtr-view.component.html'
+})
+
+export class MtrViewComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
