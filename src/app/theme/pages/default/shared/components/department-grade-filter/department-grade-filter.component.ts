e11624d02eb80145cc5253351da73cb3740ef8f6
Murtaza <murtazamunshi@live.com>
2018-12-24 23:34:44 +0530

    Added shared components

---
+++ src/app/theme/pages/default/shared/components/department-grade-filter/department-grade-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'department-grade-filter',
+    templateUrl: 'department-grade-filter.component.html'
+})
+
+export class DepartmentGradeFilterComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ src/app/theme/pages/default/shared/components/employee-batch-selection-grid/employee-batch-selection-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'employee-batch-selection-grid',
+    templateUrl: 'employee-batch-selection-grid.component.html'
+})
+
+export class EmployeeBatchSelectionGridComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
