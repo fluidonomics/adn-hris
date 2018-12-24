8e9601baf4655706e3990c5c26d79558fca2d9f8
Murtaza <murtazamunshi@live.com>
2018-12-24 23:19:38 +0530

    Added components for PAP Batch

---
+++ src/app/theme/pages/default/hr/workFlows/pap/batch/initiate/pap-batch-init.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation, OnInit } from "@angular/core";
+
+
+@Component({
+    selector: '.m-grid__item.m-grid__item--fluid.m-wrapper.papinit',
+    templateUrl: 'pap-batch-init.component.html',
+    encapsulation: ViewEncapsulation.None,
+})
+
+export class PapBatchInitComponent implements OnInit {
+
+
+}
---
+++ src/app/theme/pages/default/hr/workFlows/pap/batch/view/pap-batch-view.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation, OnInit } from "@angular/core";
+
+
+@Component({
+    selector: '.m-grid__item.m-grid__item--fluid.m-wrapper.papBatchView',
+    templateUrl: 'pap-batch-view.component.html',
+    encapsulation: ViewEncapsulation.None,
+})
+
+export class PapBatchViewComponent implements OnInit {
+
+
+}
