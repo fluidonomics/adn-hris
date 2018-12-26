aeab0be211207f276c6c2a254a55c2a493133d14
Rohan Bhardwaj <rohanbd12391@gmail.com>
2018-12-27 02:23:20 +0530

    fixes

--- src/app/theme/pages/default/my/workflows/pap/pap.component.ts
+++ src/app/theme/pages/default/my/workflows/pap/pap.component.ts
@@ -19,6 +19,29 @@ export class MyPapComponent {
     papWorkFlowData: any = [];
     isPapAvaliable: boolean = false;
 
+    supervisorData: any = [];
+    weightageData: any = [];
+    papCategoryData:any=[];
+    papRatingScaleData:any=[];
+
+    papData: any = {};
+
+    progressStatuses = [
+        {
+            id: "InProgress",
+            label: "In progress"
+        },
+        {
+            id: "Completed",
+            label: "Completed"
+        },
+        {
+            id: "Dropped",
+            label: "Dropped"
+        }
+    ];
+   
+
     modalRef: BsModalRef;
     param_id: number;
     _currentEmpId: number;
@@ -31,12 +54,15 @@ export class MyPapComponent {
 
     papMasterData = [];
 
+    papInfoData:any=[];
+
     constructor(
         private _route: ActivatedRoute,
         private _router: Router,
         public _authService: AuthService,
         private _commonService: CommonService,
-        private papService: PapService
+        private papService: PapService,
+        private modalService: BsModalService,
     ) { }
 
     ngOnInit() {
@@ -54,21 +80,79 @@ export class MyPapComponent {
             });
         });
     }
-
-    loadData() {
-
+    loadWeightAgeData() {
+        this._commonService.getKraWeightage()
+            .subscribe(
+                data => {
+                    this.weightageData = data.json();
+                },
+                error => {
+                });
+    }
+    loadRatingScaleData(){
+        this._commonService.getPapRatingScale().subscribe(
+            data => {
+                this.papRatingScaleData = data.json().result;
+                this.papRatingScaleData.forEach(element => {
+                    element.displayName=element.ratingScale+"-"+element.nomenclature
+                });
+                console.log(this.papRatingScaleData);
+            }, error => {
+            });
+    }
+    loadSupervisorData() {
+        this._commonService.getKraSupervisor(this._currentEmpId).subscribe(data => {
+            this.supervisorData = data.json();
+        }, error => {
+        });
     }
 
+    loadData() {
+        this.loadPapDetails();
+        this.loadSupervisorData();
+        this.loadWeightAgeData();
+        this.loadPAPCategoryData();
+        this.loadRatingScaleData();
+    }
+    loadPAPCategoryData() {
+        this._commonService.getKraCategory().subscribe(data => {
+            this.papCategoryData = data.json();
+        }, error => {
+        });
+    }
     loadPapDetails() {
-        this.papService.getPapDetailsSingleEmployee(this._currentEmpId).subscribe(res => {
-            debugger;
+        this.papService.getPapDetailsSingleEmployee(this._currentEmpId).subscribe(res => {            
             let papDetails = res || [];
             if (papDetails.length > 0) {
-                this.papMasterData = _.chain(papDetails).groupBy('pap_master_id').map(function (v, i) {
+                this.papWorkFlowData = _.chain(papDetails).groupBy('pap_master_id').map(function (v, i) {
                     return v[0];
                 }).value();
+                this.papInfoData=this.papWorkFlowData[0].papdetails;
+                console.log(this.papWorkFlowData);
             }
         });
+    }
+    showPAPDetails(index: number) {
+        this.modalRef = this.modalService.show(this.papDetailModal, Object.assign({}, { class: 'gray modal-lg' }));
+        this.papData = JSON.parse(JSON.stringify(this.papInfoData[index]));
+        this.papData.no = index + 1;
+        // if (this.papData.kra_details && this.papData.kra_details._id) {
+        //     this.isPreviousKRA = true;
+        // }
+        // else {
+        //     this.isPreviousKRA = false;
+        // }
+        // if (this.papData.progressStatus == "Dropped") {
+        //     this.isDisabled = true;
+        // } else {
+        //     if (this.mtrData.status) {
+        //         this.isDisabled = this.mtrData.status == "SendBack" || this.mtrData.status == "Pending" ? false : true;
+        //     }
+        // }
+
+
+        //this.mtrData.weightage = this.weightageData.find(f => f._id == this.mtrData.weightage_id);
+        //this.mtrData.category = this.kraCategoryData.find(f => f._id == this.kraData.category_id);
     }
 
 
