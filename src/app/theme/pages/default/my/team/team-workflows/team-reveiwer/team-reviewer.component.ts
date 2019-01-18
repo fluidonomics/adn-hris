08f55415343f29b79ed45560a7c3054535854dc7
Rohan Bhardwaj <rohanbd12391@gmail.com>
2019-01-19 01:40:12 +0530

    fixes

--- src/app/theme/pages/default/my/team/team-workflows/team-reveiwer/team-reviewer.component.ts
+++ src/app/theme/pages/default/my/team/team-workflows/team-reveiwer/team-reviewer.component.ts
@@ -71,14 +71,14 @@ export class MyTeamReviewerComponent implements OnInit {
                     if (moment(a.kra.updatedAt).isBefore(b.kra.updatedAt)) return 1;
                     else if (!moment(a.kra.updatedAt).isBefore(b.kra.updatedAt)) return -1;
                     else return 0;
-                });
+                });               
                 this.employees = this.employees.filter(a => a.kra.status == 'Submitted' || a.kra.status == 'Approved')
 
             }
         })
     }
 
-    goToKraReview(kra) {
+    goToKraReview(kra) {       
         this.router.navigateByUrl('my/team/workflows/kra-review/' + kra._id + '/' + kra.emp_id);
     }
     goToMtrReview(employee) {
