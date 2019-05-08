0edae94e99bec463a425b3aefd9d61e05fb1f54e
dhirajkpathak <dhiraj.pathak007@gmail.com>
2019-05-08 18:30:34 +0530

    HR dashboard bug

--- src/app/theme/pages/default/hr/dashboard/dashboard.component.ts
+++ src/app/theme/pages/default/hr/dashboard/dashboard.component.ts
@@ -115,7 +115,14 @@ downloadKraCsv() {
               row.push(this.empKradata.result.message[i][columnArr[0]][columnArr[1]])  
             }
             else{
-               row.push(this.empKradata.result.message[i][head]);
+                if(head == 'empName') {
+                    row.push(this.empKradata.result.message[i][head] + "(" + this.empKradata.result.message[i]["empId"] + ")");
+                } else if(head == 'supname') {
+                    row.push(this.empKradata.result.message[i][head] + "(" + this.empKradata.result.message[i]["supId"] + ")");
+                } else {
+                    row.push(this.empKradata.result.message[i][head]);
+                }
+               
             }
          }
          csv.push(row.join(","));
@@ -126,7 +133,8 @@ downloadKraCsv() {
 
 
 downloadMtrCsv() {
-
+    
+    
     let csvHeader=[];
     let filedList=[];
     let dashboardName = "";
@@ -155,12 +163,19 @@ downloadMtrCsv() {
               row.push(this.empKradata.result.message[i][columnArr[0]][columnArr[1]])  
             }
             else{
-               row.push(this.empKradata.result.message[i][head]);
+
+                if(head == 'empName' && this.empKradata.result.message[i][head]) {
+                    row.push(this.empKradata.result.message[i][head] + "(" + this.empKradata.result.message[i]["empId"] + ")");
+                } else if(head == 'supname' && this.empKradata.result.message[i][head]) {
+                    row.push(this.empKradata.result.message[i][head] + "(" + this.empKradata.result.message[i]["supId"] + ")");
+                } else {
+                    row.push(this.empKradata.result.message[i][head]);
+                }
             }
          }
          csv.push(row.join(","));
      }
-     this.utilityService.saveAsCSV(csv.join("\n"), dashboardName)
+     this.utilityService.saveAsCSV(csv.join("\n"), dashboardName);
     
 }
 
@@ -244,7 +259,6 @@ getDashboard() {
             }
         })
      } else if(this.dashboardFilter.dashboard && this.dashboardFilter.dashboard == "MTR") {
-
             this._hrService.getMtrDetails(this.dashboardFilter.date[0], this.dashboardFilter.date[1]).subscribe(res => {
                 if (res.ok) {
                     let hrMtrdata = res.json() || [];
@@ -263,6 +277,8 @@ getDashboard() {
             })
      } else if(this.dashboardFilter.dashboard && this.dashboardFilter.dashboard == "Learning") {
 
+        this.empKradata = {};
+        
         this._hrService.getLearningDetails(this.dashboardFilter.date[0], this.dashboardFilter.date[1]).subscribe(res => {
             if (res.ok) {
                 let hrLearningdata = res.json() || [];
