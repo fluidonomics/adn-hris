83d5eeccdf0e3a1d0df219d55907a3284a3ffd67
dhirajkpathak <dhiraj.pathak007@gmail.com>
2019-05-18 12:04:00 +0530

    dashboard presentation

--- src/app/theme/pages/default/dashboard/dashboard.component.ts
+++ src/app/theme/pages/default/dashboard/dashboard.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, ViewEncapsulation, Inject, PLATFORM_ID, AfterViewIni
 import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { Meta, Title } from "@angular/platform-browser";
 import { Helpers } from '../../../../helpers';
+import { DocumentService } from "../../../../base/_services/documentService.service";
 import { ScriptLoaderService } from '../../../../_services/script-loader.service';
+import * as FileSaver from 'file-saver';
 
 
 @Component({
@@ -12,8 +14,12 @@ import { ScriptLoaderService } from '../../../../_services/script-loader.service
 })
 export class DashboardComponent implements OnInit, AfterViewInit {
 
+    fileUrl: String = "adn-test-bucket/externalDocuemnt/PMS+Presentation+-+180918.pptx";
+    fileName: String = "performance_management";
+
     constructor( @Inject(PLATFORM_ID) private platformId: Object,
         meta: Meta, title: Title,
+        private _documentService: DocumentService,
         private _script: ScriptLoaderService,
     ) {
         title.setTitle('ADN Dashbord | Dashboard');
@@ -30,6 +36,16 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     ngAfterViewInit() {
         this._script.load('.m-grid__item.m-grid__item--fluid.m-wrapper',
             'assets/app/js/dashboard.js');
+    }
+
+    downloadPdf(fileUrl, fileName) {
+        this._documentService.downloadPpt(fileUrl).subscribe((response) => {
+            var mediaType = 'application/pptx';
+            var blob = new Blob([response['_body']], { type: mediaType });
+            //var filename = fileName.split(' ').join('_') + '_' + '.pdf';
+            var filename = "performance_management" + '.pptx';
+            FileSaver.saveAs(blob, filename);
+        });
     }
 
 }
