6a978af1b38f7707f3bac04faae2f572715e590e
AmitBisht <Bishtamith@gmail.com>
2019-11-02 22:46:50 +0530

    initial code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,9 +6,9 @@ import { ScriptLoaderService } from "../_services/script-loader.service";
 import { AuthService } from "../base/_services/authService.service";
 import { Helpers } from "../helpers";
 import { CommonService } from "../base/_services/common.service";
-
-
-
+import * as pdfMake from 'pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
     selector: ".m-grid.m-grid--hor.m-grid--root.m-page",
     templateUrl: './login.component.html',
@@ -16,53 +16,69 @@ import { CommonService } from "../base/_services/common.service";
 })
 
 export class LoginComponent implements OnInit {
-    loading = false;
-    returnUrl: string;
-    loginModel: any = {
 
-    };
-    error = false;
-    errorMsg = "";
-    // @ViewChild('loginForm') loginForm: ElementRef;
-    constructor( @Inject(PLATFORM_ID) private platformId: Object,
-        meta: Meta, title: Title,
-        private _route: ActivatedRoute,
-        public _router: Router,
-        private _authService: AuthService,
-        private _commonService: CommonService
-    ) {
-        title.setTitle('ADN HRIS | Login');
-        meta.addTags([
-            { name: 'author', content: 'LogiN Adn' },
-            { name: 'keywords', content: 'Login.' },
-            { name: 'description', content: 'Login.' }
-        ]);
-        // setTimeout(function(){window.location.reload()}, 1000);
-        // clearTimeout();
+    constructor( ) {
     }
+    prepareBody(): any {
+        let headerData = [];
+        headerData = [
+            { text: 'KEY RESPONSIBILITY AREAS (KRAs)', style: 'tableHeader', colSpan: 11, alignment: 'center' },
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {}
+        ];
+        let subHeaderData = [];
+        subHeaderData = [
+            { text: 'S.No.', style: 'tableHeader', alignment: 'center' },
+            { text: 'KRA DESCRIPTION*', style: 'tableHeader', alignment: 'center' },
+            { text: 'CATEGORIZATION*', style: 'tableHeader', alignment: 'center' },
+            { text: 'WEIGHTAGE*[in %]', style: 'tableHeader', alignment: 'center' },
+            { text: 'Metrics ', style: 'tableHeader', alignment: 'center' },
+            { text: 'KRA Measure of Success', style: 'tableHeader', alignment: 'center' },
+            { text: 'Year End Review', style: 'tableHeader', alignment: 'center' },
+            { text: 'Employee Comments', style: 'tableHeader', alignment: 'center' },
+            { text: 'Managers Comment', style: 'tableHeader', alignment: 'center' },
+            { text: 'Reviewers Comment', style: 'tableHeader', alignment: 'center' },
+            { text: 'Final Performance Rating', style: 'tableHeader', alignment: 'center' }
+        ];
+        let bodyData = [];
+        bodyData.push(headerData);
+        bodyData.push(subHeaderData);
 
-
+        return bodyData;
+    }
+    preparePdf(){
+        let bodyData = this.prepareBody();
+        var dd = {
+            pageOrientation: 'landscape',
+            pageSize: 'LEGAL',
+            content: [
+                {
+                    style: 'tableExample',
+                    color: '#444',
+                    table: {
+                        widths: ['auto', 'auto', 'auto', 'auto', 'auto', 'auto'
+                            , 'auto', 'auto', 'auto', 'auto', 'auto'
+                        ],
+                        headerRows: 2,
+                        // keepWithHeaderRows: 1,
+                        body: bodyData
+                    }
+                }
+            ]
+          };
+        pdfMake.createPdf(dd).download();
+    }
     ngOnInit() {
-        //this.remember = true;
-        this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/';
-        this._router.navigate([this.returnUrl]);
-        Helpers.setLoading(false);
+        this.preparePdf();
     }
 
-    login() {
-        this.loading = true;
-        this._authService.login(this.loginModel)
-            .subscribe(
-            data => {
-
-                this._commonService.setCompanyIdLocal(data.json().company_id);
-                this._router.navigate([this.returnUrl]);
-            },
-            error => {
-                this.errorMsg = error.json().error.message || "Error to login";
-                //this.errorMsg = "Error to login";
-                this.error = true;
-                this.loading = false;
-            });
-    }
+   
 }
