66dc8f1bef4b8a686f2105fd4137b878c92898f1
AmitBisht <Bishtamith@gmail.com>
2019-11-05 00:49:24 +0530

    footer, style intgrated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,7 +22,8 @@ export class LoginComponent implements OnInit {
     prepareBody(): any {
         let headerData = [];
         headerData = [
-            { text: 'KEY RESPONSIBILITY AREAS (KRAs)', style: 'tableHeader', colSpan: 11, alignment: 'center' },
+            { text: 'KEY RESPONSIBILITY AREAS (KRAs)', style: 'toptableHeader', colSpan: 12, fillColor: '#cccccc', alignment: 'center' },
+            {},
             {},
             {},
             {},
@@ -36,22 +37,28 @@ export class LoginComponent implements OnInit {
         ];
         let subHeaderData = [];
         subHeaderData = [
-            { text: 'S.No.', style: 'tableHeader', alignment: 'center' },
-            { text: 'KRA DESCRIPTION*', style: 'tableHeader', alignment: 'center' },
-            { text: 'CATEGORIZATION*', style: 'tableHeader', alignment: 'center' },
-            { text: 'WEIGHTAGE*[in %]', style: 'tableHeader', alignment: 'center' },
-            { text: 'Metrics ', style: 'tableHeader', alignment: 'center' },
-            { text: 'KRA Measure of Success', style: 'tableHeader', alignment: 'center' },
-            { text: 'Year End Review', style: 'tableHeader', alignment: 'center' },
-            { text: 'Employee Comments', style: 'tableHeader', alignment: 'center' },
-            { text: 'Managers Comment', style: 'tableHeader', alignment: 'center' },
-            { text: 'Reviewers Comment', style: 'tableHeader', alignment: 'center' },
-            { text: 'Final Performance Rating', style: 'tableHeader', alignment: 'center' }
+            { text: 'S.No.', style: 'subtableHeader', alignment: 'center' },
+            { text: 'KRA DESCRIPTION*', style: 'subtableHeader', alignment: 'center' },
+            { text: 'CATEGORIZATION*', style: 'subtableHeader', alignment: 'center' },
+            { text: 'WEIGHTAGE* [in %]', style: 'subtableHeader', alignment: 'center' },
+            { text: 'Metrics ', style: 'subtableHeader', alignment: 'center' },
+            { text: 'KRA Measure of Success', style: 'subtableHeader', alignment: 'center' },
+            { text: 'Year End Review (Self Appraisal)', style: 'subtableHeader', alignment: 'center' },
+            { text: 'Year End Review (Manager Rating)', style: 'subtableHeader', alignment: 'center' },
+            { text: 'Employee Comments', style: 'subtableHeader', alignment: 'center' },
+            { text: 'Managers Comment', style: 'subtableHeader', alignment: 'center' },
+            { text: 'Reviewers Comment', style: 'subtableHeader', alignment: 'center' },
+            { text: 'Final Performance Rating', style: 'subtableHeader', alignment: 'center' }
         ];
         let bodyData = [];
         bodyData.push(headerData);
         bodyData.push(subHeaderData);
-        bodyData.push(['Sample value 1', '', '', '', '', '', '','','','', '']);
+        bodyData.push(['1', 'ADN Group Network Security Manage and Monitor', 'Functional/Customer Deliverables', '15%', '', 'About 88 Tickets closed', '20', '20','Approx Invoice Submission BDT 50 Crore; Collection BDT 49.45 Crore; 98...','Good number new clients last year.','Good number new clients last year.', '4']);
+        bodyData.push(['2', 'ADN Group Network Security Manage and Monitor', 'Functional/Customer Deliverables', '15%', '', 'About 88 Tickets closed', '20', '20','Approx Invoice Submission BDT 50 Crore; Collection BDT 49.45 Crore; 98...','Good number new clients last year.','Good number new clients last year.', '4']);
+        bodyData.push(['3', 'ADN Group Network Security Manage and Monitor', 'Functional/Customer Deliverables', '15%', '', 'About 88 Tickets closed', '20', '20','Approx Invoice Submission BDT 50 Crore; Collection BDT 49.45 Crore; 98...','Good number new clients last year.','Good number new clients last year.', '4']);
+        bodyData.push(['4', 'ADN Group Network Security Manage and Monitor', 'Functional/Customer Deliverables', '15%', '', 'About 88 Tickets closed', '20', '20','Approx Invoice Submission BDT 50 Crore; Collection BDT 49.45 Crore; 98...','Good number new clients last year.','Good number new clients last year.', '4']);
+        bodyData.push(['5', 'ADN Group Network Security Manage and Monitor', 'Functional/Customer Deliverables', '15%', '', 'About 88 Tickets closed', '20', '20','Approx Invoice Submission BDT 50 Crore; Collection BDT 49.45 Crore; 98...','Good number new clients last year.','Good number new clients last year.', '4']);
+        bodyData.push([{ colSpan: 9, text: '' }, '', '', '', '', '', '', '', '', { colSpan: 2, text: 'Fill Over All Rating', style: 'tableFooter' }, '', { text: '5', fontSize: 12 }]);
         return bodyData;
     }
     preparePdf(){
@@ -63,16 +70,32 @@ export class LoginComponent implements OnInit {
                 {
                     style: 'tableExample',
                     color: '#444',
+                    fontSize: 9,
+                    alignment: 'center',
                     table: {
-                        widths: ['auto', 'auto', 'auto', 'auto', 'auto', 'auto'
-                            , 'auto', 'auto', 'auto', 'auto', 'auto'
+                        widths: [30, 'auto', 'auto', 70, 'auto', 'auto',
+                            50, 50, 'auto', 'auto', 'auto', 60
                         ],
                         headerRows: 2,
                         // keepWithHeaderRows: 1,
                         body: bodyData
                     }
                 }
-            ]
+            ],
+            styles: {
+                toptableHeader: {
+                    bold: true,
+                    fontSize: 14
+                },
+                subtableHeader: {
+                    bold: true,
+                    fontSize: 10
+                },
+                tableFooter: {
+                    bold: true,
+                    fontSize: 12
+                }
+            }
           };
         pdfMake.createPdf(dd).download();
     }
