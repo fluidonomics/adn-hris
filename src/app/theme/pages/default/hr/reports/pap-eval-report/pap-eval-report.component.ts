781841060e983784dddad0f0e161a8fabb5a5bff
Murtaza <murtazamunshi@live.com>
2019-11-05 16:45:55 +0530

    Added pdfMake Code

--- src/app/theme/pages/default/hr/reports/pap-eval-report/pap-eval-report.component.ts
+++ src/app/theme/pages/default/hr/reports/pap-eval-report/pap-eval-report.component.ts
@@ -2,6 +2,9 @@ import { Router } from '@angular/router';
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { HrService } from "../../hr.service";
 
+import * as pdfMake from 'pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
     selector: ".m-grid__item.m-grid__item--fluid.m-wrapper-pap-eval-report",
@@ -36,8 +39,63 @@ export class PapEvalReport implements OnInit {
         })
     }
 
-    downloadReport(empId){
+    downloadReport(empId) {
+        let bodyData = this.prepareBody();
+        var dd = {
+            pageOrientation: 'landscape',
+            pageSize: 'LEGAL',
+            content: [
+                {
+                    style: 'tableExample',
+                    color: '#444',
+                    table: {
+                        widths: ['auto', 'auto', 'auto', 'auto', 'auto', 'auto'
+                            , 'auto', 'auto', 'auto', 'auto', 'auto'
+                        ],
+                        headerRows: 2,
+                        // keepWithHeaderRows: 1,
+                        body: bodyData
+                    }
+                }
+            ]
+        };
+        pdfMake.createPdf(dd).download();
+    }
 
+    prepareBody(): any {
+        let headerData = [];
+        headerData = [
+            { text: 'KEY RESPONSIBILITY AREAS (KRAs)', style: 'tableHeader', colSpan: 11, alignment: 'center' },
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {}
+        ];
+        let subHeaderData = [];
+        subHeaderData = [
+            { text: 'S.No.', style: 'tableHeader', alignment: 'center' },
+            { text: 'KRA DESCRIPTION*', style: 'tableHeader', alignment: 'center' },
+            { text: 'CATEGORIZATION*', style: 'tableHeader', alignment: 'center' },
+            { text: 'WEIGHTAGE*[in %]', style: 'tableHeader', alignment: 'center' },
+            { text: 'Metrics ', style: 'tableHeader', alignment: 'center' },
+            { text: 'KRA Measure of Success', style: 'tableHeader', alignment: 'center' },
+            { text: 'Year End Review', style: 'tableHeader', alignment: 'center' },
+            { text: 'Employee Comments', style: 'tableHeader', alignment: 'center' },
+            { text: 'Managers Comment', style: 'tableHeader', alignment: 'center' },
+            { text: 'Reviewers Comment', style: 'tableHeader', alignment: 'center' },
+            { text: 'Final Performance Rating', style: 'tableHeader', alignment: 'center' }
+        ];
+        let bodyData = [];
+        bodyData.push(headerData);
+        bodyData.push(subHeaderData);
+
+        return bodyData;
     }
 
     sort(key) {
